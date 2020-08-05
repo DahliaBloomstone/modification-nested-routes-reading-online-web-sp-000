3dcda92a0f83f52a94e748c8bf13aeb7c6c0e768
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-08-05 19:15:41 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -46,7 +46,7 @@ if params[:author_id] #comes from our nested route
   if author.nil?
     redirect_to authors_path, alert: "Author not found." #flash alert if can't find valid author
   else
-    @post = author.posts.find_by(id: params[:id])
+    @post = author.posts.find_by(id: params[:id]) #if author valid, find post by params[:id]
     redirect_to author_posts_path(author), alert: "Post not found." if @post.nil?
   end
 else
